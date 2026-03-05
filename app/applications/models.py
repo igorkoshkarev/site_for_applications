80254eda69aa7c28ad31992c91fdaf7744f78657
igorkoshkarev <igor.koshkarev.04@gmail.com>
2026-03-05 22:47:01 +0700

    create alembic migrations and fix some errors in models

--- app/applications/models.py
+++ app/applications/models.py
@@ -1,14 +1,10 @@
-import app.base as base
+import app.database as database
 from sqlalchemy.orm import mapped_column, Mapped
 from sqlalchemy import Text, ForeignKey
-from enum import Enum
 
 
-
-class Application(base.Base):
-    id: Mapped[base.int_pk]
+class Application(database.Base):
+    id: Mapped[database.int_pk]
     title: Mapped[str] = mapped_column(Text, nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=True)
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
-
-
---
+++ app/users/models.py
@@ -0,0 +1,21 @@
+from app.database import Base, int_pk
+from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import Text, ForeignKey
+
+
+class User(Base):
+    id: Mapped[int_pk]
+    username: Mapped[str] = mapped_column(Text, nullable=False)
+    role_id: Mapped[int] = mapped_column(ForeignKey('roles.id'), nullable=False)
+
+
+class Role(Base):
+    id: Mapped[int_pk]
+    role: Mapped[str]
+    law_create_applications: Mapped[bool]
+    law_update_applications: Mapped[bool]
+    law_delete_applications: Mapped[bool]
+    law_create_users: Mapped[bool]
+    law_update_users: Mapped[bool]
+    law_delete_users: Mapped[bool]
+
