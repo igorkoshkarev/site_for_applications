e7db89bf4d4f57260fd407acc9d5d77908abd9ba
igorkoshkarev <igor.koshkarev.04@gmail.com>
2026-03-23 21:21:23 +0700

    add status and performer columns in application table

--- app/applications/models.py
+++ app/applications/models.py
@@ -1,12 +1,23 @@
 import app.database as database
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy import Text, ForeignKey
+from enum import Enum, auto
+
+
+class ApplicationStatus(str, Enum):
+    is_open: str = auto()
+    in_process: str = auto()
+    is_closed: str = auto()
+    is_confirmed: str = auto()
 
 
 class Application(database.Base):
     id: Mapped[database.int_pk]
     title: Mapped[str] = mapped_column(Text, nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=True)
+    status: Mapped[ApplicationStatus] = mapped_column(server_default=ApplicationStatus.is_open, nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+    performer_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=True)
 
-    user = relationship('User', back_populates='applications')
+    user = relationship('User', foreign_keys=[user_id], back_populates='applications')
+    performer = relationship('User', foreign_keys=[performer_id], back_populates="preformed_applications")
--- app/users/models.py
+++ app/users/models.py
@@ -13,7 +13,8 @@ class User(Base):
     role_name: Mapped[int] = mapped_column(ForeignKey('roles.role'), nullable=False)
 
     role = relationship('Role', back_populates='users')
-    applications = relationship("Application", back_populates="user")
+    applications = relationship("Application", foreign_keys='[Application.user_id]', back_populates="user")
+    performed_applications = relationship('Application', foreign_keys='[Application.performer_id]', back_populates='performer')
 
 
 class Role(Base):
